19d9de1f55dd6aae990280b840c02623b5b334b0
robert <siara.robert@gmail.com>
2018-06-24 01:59:33 +0200

    Init

---
+++ src/main/java/graphqlsandbox/graphqlsandbox/GraphqlSandboxApplication.java
@@ -0,0 +1,12 @@
+package graphqlsandbox.graphqlsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSandboxApplication.class, args);
+    }
+}
---
+++ src/test/java/graphqlsandbox/graphqlsandbox/GraphqlSandboxApplicationTests.java
@@ -0,0 +1,16 @@
+package graphqlsandbox.graphqlsandbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphqlSandboxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
